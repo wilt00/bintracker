4f9e8f32394887155ebbea0fe21f7bf2068e5479
WT <toherwils@gmail.com>
2026-09-21 13:28:59 -0700

    add Windows release automation

--- config/emulators.windows.scm
+++ config/emulators.windows.scm
@@ -1,5 +1,5 @@
 
-((mame program-name: "3rdparty\\mame\\mame.exe"
+((mame program-name: "mame.exe"
        default-args: ("-w" "-skip_gameinfo" "-autoboot_script"
 		      "mame-bridge/mame-startup.lua"
 		      "-rompath" "roms"
